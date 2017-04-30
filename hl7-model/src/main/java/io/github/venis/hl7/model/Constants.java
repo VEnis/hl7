ec918bb84d46faf2a782f6dce3261e66a2bbdeea
Vyacheslav Enis <vyacheslav.enis@tonicforhealth.com>
2017-04-30 13:59:48 +0300

    Fixed some issues

--- hl7-model/src/main/java/io/github/venis/hl7/model/Constants.java
+++ hl7-model/src/main/java/io/github/venis/hl7/model/Constants.java
@@ -1,6 +1,10 @@
 package io.github.venis.hl7.model;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class Constants {
     static final int DEFAULT_COMPONENT_SUB_COMPONENT_NUMBER = 1;
     static final int DEFAULT_REPETITION_NUMBER = 0;
+    static final String DEFAULT_PROCESSING_ID = "P";
 }
--- hl7-model/src/main/java/io/github/venis/hl7/model/Message.java
+++ hl7-model/src/main/java/io/github/venis/hl7/model/Message.java
@@ -1,51 +1,69 @@
 package io.github.venis.hl7.model;
 
 import ca.uhn.hl7v2.HL7Exception;
+import ca.uhn.hl7v2.Version;
+import ca.uhn.hl7v2.model.AbstractMessage;
+import ca.uhn.hl7v2.parser.DefaultModelClassFactory;
+import ca.uhn.hl7v2.util.ReflectionUtil;
 import ca.uhn.hl7v2.util.Terser;
-import lombok.NonNull;
 import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.Serializable;
 
-@ToString(callSuper = true, of = "")
+@ToString(callSuper = true, exclude = {"terserMessage"})
+@Slf4j
 public class Message extends Group implements Serializable {
-    @NonNull private transient Terser terserMessage;
+    private transient Terser terserMessage;
 
     public Message(ca.uhn.hl7v2.model.Message hapiMessage) {
         super(hapiMessage);
         this.terserMessage = new Terser(hapiMessage);
     }
 
-//    public Message(String version, String type, String processingId) {
-//        this(createMessage(version, type, processingId));
-//    }
-//
-//    private static ca.uhn.hl7v2.model.Message createMessage(String version, String type, String processingId) {
-//        String[] messageTypeParts = type.split("_");
-//        if (messageTypeParts.length != 2) throw new IllegalArgumentException("Passed hl7 type is invalid: " + type);
-//
-//        DefaultModelClassFactory defaultModelClassFactory = new DefaultModelClassFactory();
-//
-//        try {
-//            Class<? extends ca.uhn.hl7v2.model.Message> messageClass = defaultModelClassFactory.getMessageClass(type, version, false);
-//            ca.uhn.hl7v2.model.Message message = ReflectionUtil.instantiateMessage(messageClass, defaultModelClassFactory);
-//
-//            String part1 = messageTypeParts[0];
-//            String part2 = messageTypeParts[1];
-//
-//            ((AbstractMessage) message).initQuickstart(part1, part2, processingId);
-//            return message;
-//        } catch (HL7Exception | IOException e) {
-//            throw new IllegalArgumentException(e.getMessage(), e);
-//        }
-//    }
+    public Message(String type, Version version) {
+        this(type, version.getVersion());
+    }
+
+    public Message(String type, String version) {
+        this(type, version, Constants.DEFAULT_PROCESSING_ID);
+    }
+
+    public Message(String type, Version version, String processingId) {
+        this(type, version.getVersion(), processingId);
+    }
+
+    public Message(String type, String version, String processingId) {
+        this(createMessage(type, version, processingId));
+    }
+
+    private static ca.uhn.hl7v2.model.Message createMessage(String type, String version, String processingId) {
+        String[] messageTypeParts = type.split("_");
+        if (messageTypeParts.length != 2) throw new IllegalArgumentException("Passed hl7 type is invalid: " + type);
+
+        DefaultModelClassFactory defaultModelClassFactory = new DefaultModelClassFactory();
+
+        try {
+            Class<? extends ca.uhn.hl7v2.model.Message> messageClass = defaultModelClassFactory.getMessageClass(type, version, false);
+            ca.uhn.hl7v2.model.Message message = ReflectionUtil.instantiateMessage(messageClass, defaultModelClassFactory);
+
+            String part1 = messageTypeParts[0];
+            String part2 = messageTypeParts[1];
+
+            ((AbstractMessage) message).initQuickstart(part1, part2, processingId);
+            return message;
+        } catch (HL7Exception | IOException e) {
+            throw new IllegalArgumentException(e.getMessage(), e);
+        }
+    }
 
     public String getByTerser(String terserSpecification) {
         try {
             return terserMessage.get(terserSpecification);
         } catch (HL7Exception e) {
+            log.debug("No result extracted by terser specification", e);
             return null;
         }
     }
